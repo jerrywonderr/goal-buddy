0bd977fdc73a608ba41ec03f0dc000da835918bd
Jerry Wonder <jerrycul2001@gmail.com>
2023-03-29 16:29:54 +0100

    feat(endpoint): add logic to get all groups a user belongs to

--- backend/src/modules/main/group/group.controller.ts
+++ backend/src/modules/main/group/group.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Controller,
   Delete,
+  ForbiddenException,
   Get,
   HttpCode,
   HttpStatus,
@@ -11,6 +12,7 @@ import {
 } from '@nestjs/common';
 import { MainService } from '../main.service';
 import { TaskService } from '../task/task.service';
+import { UserService } from '../user/user.service';
 import { CreateGroupDto } from './dto/creategroup.dto';
 import { GroupService } from './group.service';
 
@@ -19,16 +21,19 @@ export class GroupController {
   constructor(
     private readonly groupService: GroupService,
     private readonly mainService: MainService,
-    private readonly taskService: TaskService
+    private readonly taskService: TaskService,
+    private readonly userService: UserService
   ) {}
 
+  /**
+   * Get all groups attached to a user
+   */
   @Get()
-  getAll() {
-    /**
-     * Get all groups attached to a user
-     */
-
-    return [];
+  async getUserGroups() {
+    const username = 'wonder1';
+    const user = await this.userService.get(username);
+    if (!user) throw new ForbiddenException('No current session');
+    return user.groups.map(value => value.group);
   }
 
   @Delete()
